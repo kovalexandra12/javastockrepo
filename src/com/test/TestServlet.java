3b05b7763ec7c0261138f7ccf0222558a04439a7
kovalexandra12 <kovalexandra1@gmail.com>
2015-04-05 21:32:06 +0300

    EX2 math calcultaions

--- src/com/test/TestServlet.java
+++ src/com/test/TestServlet.java
@@ -7,8 +7,15 @@ import javax.servlet.http.*;
 public class TestServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-		int test=0;
+		resp.setContentType("text/html");
+		
+		int num1=4;
+		int num2=3;
+		int num3=7;
+		String num1Str=String.valueOf(num1);
+		int result=(num3*(num2+num1));	
+	    String resultSt1r= "<h1>Result of ("+num3+ "*("+num2+"+"+num1+"))="+result+"</h1>";
+		resp.getWriter().println(resultSt1r);
+		
 	}
 }
