ee119167772e7aa13f1da4ad770e41422f027228
kovalexandra12 <kovalexandra1@gmail.com>
2015-04-12 18:52:04 +0300

    EX3-math calculations 1.circle 2.triangle 3.power

--- src/com/test/TestServlet.java
+++ src/com/test/TestServlet.java
@@ -12,7 +12,7 @@ public class TestServlet extends HttpServlet {
 		int num1=4;
 		int num2=3;
 		int num3=7;
-		String num1Str=String.valueOf(num1);
+		
 		int result=(num3*(num2+num1));	
 	    String resultSt1r= "<h1>Result of ("+num3+ "*("+num2+"+"+num1+"))="+result+"</h1>";
 		resp.getWriter().println(resultSt1r);
